d4e0dde5b22929c9778a1dc4a73fd764aa352076
EthanYe-git <piscesyexyzc@gmail.com>
2020-10-08 18:24:21 +0800

    send broadcast with target

--- src/Main.c
+++ src/Main.c
@@ -13,6 +13,9 @@
 #define IPC_WAITFORMESSAGE 0
 #define MESSAGE_DATA_LEN 1024
 
+#define RECEIVERS_MIN 0
+#define RECEIVERS_MAX 50
+
 #define BMSG_FROM "f"
 #define BMSG_TARGET "t"
 #define BMSG_TYPE "p"
@@ -30,13 +33,16 @@ struct BroadcastMessage{
 
 static int msgID = 0;
 static key_t msgKey;
-static int receivers[50] = {0};
+static int receivers[RECEIVERS_MAX] = {0};
 
 int getMessage(struct Message *message, char *json);
+char *getMsgJson(struct Message *message);
 int handleSystem(struct Message *message);
 int handleQPlay(struct Message *message);
 int handleAirplay(struct Message *message);
 int handleSpotify(struct Message *message);
+
+int sendBroadcast(struct Message *message);
 
 int main()
 {
@@ -79,14 +85,6 @@ int main()
         default:
             break;
         }
-        /*struct BroadcastMessage sendMessage;
-		sendMessage.broadcastType = APPUID_SYSTEM;
-		strcpy(sendMessage.broadcastData, broadcast.broadcastData);
-		if(msgsnd(id,(void *)&sendMessage,MESSAGE_DATA_LEN,0) < 0)
-	    {
-		    printf("error:system send broadcast error \n");
-		    return 0;
-		}*/
 	}
 sys_exit:
     if(msgctl(msgID,IPC_RMID,NULL) < 0)
@@ -110,22 +108,52 @@ int getMessage(struct Message *message, char *json)
     return 0;
 }
 
+char *getMsgJson(struct Message *message)
+{
+    cJSON *root = cJSON_CreateObject();
+	cJSON_AddItemToObject(root, "f", cJSON_CreateNumber(message->from));
+	cJSON_AddItemToObject(root, "t", cJSON_CreateNumber(message->target));
+	cJSON_AddItemToObject(root, "p", cJSON_CreateNumber(message->type));
+	cJSON_AddItemToObject(root, "l", cJSON_CreateNumber(MESSAGE_DATA_LEN));
+	cJSON_AddItemToObject(root, "m", cJSON_CreateString(message->data));
+    char *json = strdup(cJSON_Print(root));
+    cJSON_Delete(root);
+    return json;
+}
+
+int sendBroadcast(struct Message *message)
+{
+    if(message->target < RECEIVERS_MIN || message->target >= RECEIVERS_MAX)
+        return -1;
+    if(receivers[message->target] != message->target)
+        return -1;
+    struct BroadcastMessage broadcast;
+    broadcast.broadcastType = message->target;
+    strcpy(broadcast.broadcastData, getMsgJson(message));
+
+    printf("info:data : %s\n", broadcast.broadcastData);
+    if(msgsnd(msgID,(void *)&broadcast,MESSAGE_DATA_LEN,0) < 0)
+    {
+		printf("send msg error \n");
+		return -1;
+	}
+    return 0;
+}
+
 int handleSystem(struct Message *message)
 {
+    struct Message returnMsg;
+
     switch (message->type)
     {
     case MESSAGE_TYPE_REGISTER:
-        if(message->from > 0 && message->from < 50)
-            receivers[message->from] = message->from;
-        struct BroadcastMessage sendMessage;
-		sendMessage.broadcastType = message->from;
+        receivers[message->from] = message->from;
 
-		strcpy(sendMessage.broadcastData, "system:register success");
-		if(msgsnd(msgID,(void *)&sendMessage,MESSAGE_DATA_LEN,0) < 0)
-	    {
-		    printf("error:system send broadcast error \n");
-		    return 0;
-		}
+        returnMsg.from = APPUID_SYSTEM;
+        returnMsg.target = message->from;
+        returnMsg.type = MESSAGE_TYPE_REGISTER;
+        returnMsg.data = "register success";
+        sendBroadcast(&returnMsg);
         break;
     
     default:
@@ -136,15 +164,18 @@ int handleSystem(struct Message *message)
 
 int handleQPlay(struct Message *message)
 {
+    sendBroadcast(message);
     return 0;
 }
 
 int handleAirplay(struct Message *message)
 {
+    sendBroadcast(message);
     return 0;
 }
 
 int handleSpotify(struct Message *message)
 {
+    sendBroadcast(message);
     return 0;
 }
